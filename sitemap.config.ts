1824fa01d187a70bc0f29ecf09714e7213dfc2d0
Fun117 <fun117.kun@gmail.com>
2024-04-26 03:07:45 +0900

    Add language-specific home pages to sitemap and exclude specified directories

---
+++ sitemap.config.ts
@@ -0,0 +1,6 @@
+// sitemap.config.ts
+
+export const excludedDirs = [
+    'error',
+    'not-found'
+]; // 除外したいディレクトリ名を配列で定義
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,28 +3,45 @@ import fs from 'fs';
 import path from 'path';
 import meta from '../../metadata.config';
 import i18nConfig from '../../i18n.config';
+import { excludedDirs } from '../../sitemap.config';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 	const result: MetadataRoute.Sitemap = [];
 
 	for (const lang of i18nConfig.locales) {
+		const langConfig = i18nConfig.localeConfigs[lang];
+		const homeUrl = `${meta.origin}/${langConfig.path}`;
+
+		result.push({
+			url: homeUrl,
+			lastModified: new Date().toISOString(),
+			changeFrequency: 'daily',
+			priority: 1.0,
+			alternates: {
+				languages: i18nConfig.locales.reduce<{ [key: string]: string }>((acc, l) => {
+					const langConfig = i18nConfig.localeConfigs[l];
+					acc[langConfig.htmlLang] = `${meta.origin}/${langConfig.path}`;
+					return acc;
+				}, {}),
+			},
+		});
+
 		const dirPath = path.join(process.cwd(), `src/app/[lang]`);
 
 		if (fs.existsSync(dirPath)) {
 			const items = fs.readdirSync(dirPath, { withFileTypes: true });
 
 			for (const item of items) {
-				if (item.isDirectory()) {
+				if (item.isDirectory() && !excludedDirs.includes(item.name)) {
 					const pagePath = path.join(dirPath, item.name, 'page.tsx');
 					if (fs.existsSync(pagePath)) {
-						const langConfig = i18nConfig.localeConfigs[lang];
 						const url = `${meta.origin}/${langConfig.path}/${item.name}`;
 
 						result.push({
 							url: url,
 							lastModified: new Date().toISOString(),
-							changeFrequency: 'daily', // ここで明示的に指定
-							priority: 0.7, // ここで明示的に指定
+							changeFrequency: 'daily',
+							priority: 0.7,
 							alternates: {
 								languages: i18nConfig.locales.reduce<{ [key: string]: string }>((acc, l) => {
 									const langConfig = i18nConfig.localeConfigs[l];
