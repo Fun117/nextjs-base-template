d4bdd33342f9ca62225a2a7cd6dde16d5850bf92
Fun117 <fun117.kun@gmail.com>
2024-04-25 23:56:16 +0900

    Register site with Google Search Console

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ const getNegotiatedLanguage = (
 
 export const config = {
     // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.svg|.*\\.png|.*\\.ico|.*\\.webp$).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.html|.*\\.svg|.*\\.png|.*\\.ico|.*\\.webp$).*)'],
 };
 
 export function middleware(request: NextRequest) {
